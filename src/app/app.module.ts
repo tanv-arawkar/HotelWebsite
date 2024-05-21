ce4bb7c6d0495316d3246ae0513162a41348e7c3
Tanvi Arawkar <tanviarawkar@hipro.co.in>
2024-05-21 13:35:30 +0530

    Home page completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { ImageSliderComponent } from './home/image-slider/image-slider.component';
+import { DescriptionComponent } from './home/description/description.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShareButtonsModule } from 'ngx-sharebuttons/buttons';
+import { ShareIconsModule } from 'ngx-sharebuttons/icons';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ImageSliderComponent,
+    DescriptionComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ShareButtonsModule,
+    ShareIconsModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
